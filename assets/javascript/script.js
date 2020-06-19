2679c57bf822818dc3969aaadec43da2bac38420
Ashley Holanda <aholanda12@gmail.com>
2020-06-19 15:00:04 -0400

    deleted initial prompt commentary and created organized commentary

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,6 +10,7 @@ function writePassword() {
 
 }
 
+// Variables and arrays
 var enter;
 var confirmLowercase;
 var confirmUppercase;
@@ -22,40 +23,37 @@ uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
+// Generate the password function
 function generatePassword() { 
-// THEN I am presented with a series of prompts for password criteria
-// 
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
 
+  // Initial prompt asking for password length
 enter = parseInt(prompt("How long would you like the password to be? Please choose between 8 and 128."));
 
+// Infinitely gives prompt until the criteria is met
 while (enter < 8 || enter > 128) {
   enter = parseInt(prompt("Invalid length! Please choose between 8 and 128."));
 }
 
+// Once length is validated, user is prompted to choose which character types they want in their password
 confirmLowercase = confirm("Will this contain lowercase letters?");
 confirmUppercase = confirm("Will this contain UPPERCASE letters?");
 confirmNumeric = confirm("Will this contain numbers?");
 confirmSpecial = confirm("Will this contain special characters?");
 
-// 1 PROMPT needs validation
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// Prompt needed and ask for the length
-// check IF the input number is < 8 and > 128
-// function to convert the string into a number
 
+// No character types chosen
   if (!confirmLowercase && !confirmUppercase && !confirmNumeric && !confirmSpecial) {
       choices = alert("No character type chosen. Please try again.");
 
 }
 
+// All types chosen
   else if (confirmLowercase && confirmUppercase && confirmNumeric && confirmSpecial) {
 
       choices = lowercase.concat(uppercase, numeric, special);
 }
 
+// Three types of characters chosen
   else if (!confirmLowercase && confirmUppercase && confirmNumeric && confirmSpecial) {
 
     choices = uppercase.concat(numeric, special);
@@ -73,6 +71,7 @@ confirmSpecial = confirm("Will this contain special characters?");
     choices = lowercase.concat(uppercase, numeric);
 }
 
+// Only 2 types of characters chosen
   else if (!confirmLowercase && !confirmUppercase && confirmNumeric && confirmSpecial) {
 
     choices = numeric.concat(special);
@@ -98,6 +97,7 @@ confirmSpecial = confirm("Will this contain special characters?");
     choices = lowercase.concat(uppercase);
 }
 
+// Only 1 type of character chosen
   else if (confirmLowercase && !confirmUppercase && !confirmNumeric && !confirmSpecial) {
       choices = lowercase;
 }
@@ -113,45 +113,22 @@ confirmSpecial = confirm("Will this contain special characters?");
   else if (!confirmLowercase && !confirmUppercase && !confirmNumeric && confirmSpecial) {
       choices = special;
 }
-// 4 confirms
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// Confirm needed and ask the user if they want to use lowercase
-// Confirm needed and ask the user if they want to use uppercase
-// Confirm needed and ask the user if they want to use numeric
-// Confirm needed and ask the user if they want to use special characters
+
+// variables for a push array and a string converstion
 var password = [];
 var finalProduct = "";
 
+// Randomizer created the password
 for (var i = 0; i < enter; i++) {
   var randomized = choices[Math.floor(Math.random() * choices.length)];
   password.push(randomized);
 }
 
+// Puts together the generated password into an array and returns a final product in the display (also console.log for checking)
 finalProduct = password.join("");
 console.log(finalProduct);
 return finalProduct;
 }
 
-// Need a function
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// Each prompt needs to be validated
-
-// collected data here
-// using the number from above and the 4 booleans
-
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// all the information is gathered (all prompts finished)
-// either do an alert or just check to see if my variable has a password
-// Password generation process (look at rock paper scissors)
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// Add event listener to generate button
+// Generate button
 generateBtn.addEventListener("click", writePassword);
